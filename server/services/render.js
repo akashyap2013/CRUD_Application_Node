ffe7820974ef2ada9a67c0b6d4d06e5a2cb9b709
Muhammad Naeem <m.naeem66622@outlook.com>
2022-11-22 04:37:55 +0500

    Fixed connect ECONNREFUSED ::1:3000
    Fixed error: "message":"connect ECONNREFUSED ::1:3000","name":"Error","stack":"Error: connect ECONNREFUSED ::1:3000\n    at TCPConnectWrap.afterConnect [as oncomplete] (node:net:1300:16)","config":{"url":"http://localhost:3000/api/users","method":"get","headers":{"Accept":"application/json, text/plain, */*","User-Agent":"axios/0.21.0"},"transformRequest":[null],"transformResponse":[null],"timeout":0,"xsrfCookieName":"XSRF-TOKEN","xsrfHeaderName":"X-XSRF-TOKEN","maxContentLength":-1,"maxBodyLength":-1},"code":"ECONNREFUSED"
    render.js and index.js had PORT hard cored in a code. The PORT entered in config.env rather than 3000 show error.

--- server/services/render.js
+++ server/services/render.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
+const PORT = process.env.PORT;
 
 
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://localhost:3000/api/users')
+    axios.get(`http://localhost:${PORT}/api/users`)
         .then(function(response){
             res.render('index', { users : response.data });
         })
@@ -19,7 +20,7 @@ exports.add_user = (req, res) =>{
 }
 
 exports.update_user = (req, res) =>{
-    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
+    axios.get(`http://localhost:${PORT}/api/users`, { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user", { user : userdata.data})
         })
